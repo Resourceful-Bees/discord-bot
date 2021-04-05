632f06537cfe558a84e99a424cf52d9f2e918169
James <th3darkrose@gmail.com>
2021-04-05 02:52:13 -0400

    added beekeeper and jei commands fixed broken link in lang file

--- commands/wiki/langfile.js
+++ commands/wiki/langfile.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Create a language file wiki link',
     cooldown: 5,
     execute(message) {
-        message.channel.send("https://wiki.resourcefulbees.com/en/1.16.3/getting_started/bee_creation/");
+        message.channel.send("https://wiki.resourcefulbees.com/en/1.16.3/getting_started/language_files/");
     },
 };
