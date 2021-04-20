9b8b09897e78fabf1d5d7c83ad8e5ca6a49ae971
James <th3darkrose@gmail.com>
2021-04-19 22:54:38 -0400

    added despawn alias to wherebee.js added aliases to langfile.js

--- commands/wiki/langfile.js
+++ commands/wiki/langfile.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'langfile',
     description: 'Create a language file wiki link',
     cooldown: 5,
+    aliases: ['languagefiles', 'languagefile', 'lang'],
     execute(message) {
         message.channel.send("https://wiki.resourcefulbees.com/en/1.16.3/getting_started/language_files/");
     }
